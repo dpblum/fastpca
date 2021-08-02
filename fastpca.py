45bf689d50dbb630f0cea65b0fbb997d6b4b9cad
Dominik Blum <88323153+dpblum@users.noreply.github.com>
2021-08-02 14:03:45 +0200

    Add files via upload
    Fast principal component analysis for very high dimensional data implemented according to C. Bishop's book "Pattern Recognition and Machine Learning", p. 570. For high-dimensional data, fastpca.m is much faster than Python's in-built PCA function (from sklearn.decomposition) or MATLAB's in-build function pca.m.

---
+++ fastpca.py
@@ -0,0 +1,55 @@
+import numpy as np
+from numpy import linalg as la
+
+def fastpca(data):
+    # [COEFF,SCORE,LATENT,EXPLAINED] = fastpca(data) 
+
+    # Fast principal component analysis for very high dimensional data 
+    # implemented according to C. Bishop's book "Pattern Recognition and 
+    # Machine Learning", p. 570. For high-dimensional data, fastpca.m is 
+    # much faster than Python's in-built PCA function 
+    # (from sklearn.decomposition) or MATLAB's in-build function pca.m.
+    
+    # Decrease in computation time results from calculating the PCs from the 
+    # (smaller) covariance matrix of the transposed input-matrix "data" instead 
+    # of the large covariance matrix of the original input matrix which are 
+    # then use to project the observations to achieve the PCs of the large DxD 
+    # covariance matrix. 
+ 
+    # By default, fastpca removes the mean of each observation.  In this first 
+    # implementation of fastpca, I skipped calculation of Hotelling T-Squared 
+    # Statistic as I didn't need it so far. 
+
+    # INPUT: 
+    # data = matrix of size n*p
+    # n    = number of observations
+    # p    = number of dimensions
+
+    # OUTPUT:
+    # c    = principal components, PC
+    # sc   = PC scores (projections of observations on PC's)
+    # lat  = variance captured by each PC score
+    # expl = cumulative lat in percent
+    # The p-n PC's that does not explain any variance 
+    # are automatically excluded from the results
+
+    N,D = data.shape
+    X   = data - np.mean(data,axis=0,keepdims=True)
+    
+    lats, cs = la.eig(1/(D-1)*(X@X.T))
+    latss    = np.sort(lats)[::-1]
+    css      = cs[:,np.argsort(lats)[::-1]]
+
+    css_red   = css[:,latss > 10**-10]
+    latss_red = latss[latss > 10**-10]
+
+    defl = np.zeros((len(latss_red),len(latss_red)))
+    np.fill_diagonal(defl,1/np.sqrt((D-1)*latss_red))
+    css_defl = css_red@defl
+    
+    c    = (X.T@css_defl).T
+    sc   = X@c.T
+    lat  = latss_red*(D-1)/(N-1)
+    expl = 100*latss_red/sum(latss_red)
+
+    return c, sc, lat, expl
---
+++ test_fastpca.py
@@ -0,0 +1,24 @@
+import time
+import numpy as np
+from fastpca import fastpca 
+from sklearn.decomposition import PCA
+
+# Sample data (e.g. 300 observations with 300.000 features each)
+data = np.random.rand(300,500000)
+
+# Standard PCA
+tic   = time.time()
+pca   = PCA(); 
+pca.fit(data)
+c     = pca.components_
+toc   = time.time()
+t_pca = round(toc-tic,2)
+
+# fastpca 
+tic               = time.time()
+cf, sc, lat, expl = fastpca(data)
+toc               = time.time()
+t_fastpca         = round(toc-tic,2)
+
+print('standard PCA time:' + str(t_pca))
+print('fastpca time:' + str(t_fastpca))
